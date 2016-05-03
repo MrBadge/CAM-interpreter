ec364a465098b1613143a898370302c90c71bdb7
ezio3593 <kostya3593@mail.ru>
2016-05-03 10:54:12 +0400

    Improve CAM compiler & bug fix

--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+BRACKETS = [
+    "()",
+    "<>",
+    "[]"
+    "{}"
+]
+
 class TermException(Exception):
     pass
 
@@ -30,12 +37,35 @@ def get_term_in_brackets(expr, br='()', remove_brackets=True):
 
 
 def parse_args_in_brackets(expr, br='()'):
-    br_sum = 1
+    br_sums = {}
+    for b in BRACKETS:
+        br_sums[b] = 0
+    br_sums[br] += 1
     for i, c in enumerate(expr[1:]):
+        for b in BRACKETS:
+            if c == b[0]:
+                br_sums[b] += 1
+            elif c == b[1]:
+                br_sums[b] -= 1
+        if c == ',' and br_sums[br] == 1:
+            is_balanced = True
+            for b in BRACKETS:
+                if b != br and br_sums[b] != 0:
+                    is_balanced = False
+                    break
+            if is_balanced:
+                return expr[1:i + 1], expr[i + 2:-1]
+    return None
+
+
+def is_in_brackets(expr, br='()'):
+    if expr[0] != br[0] or expr[-1] != br[1]:
+        return False
+
+    br_sum = 0
+    for c in expr:
         if c == br[0]:
             br_sum += 1
         elif c == br[1]:
             br_sum -= 1
-        elif c == ',' and br_sum == 1:
-            return expr[1:i + 1], expr[i + 2:-1]
-    return None
+    return br_sum == 0
